06c3dddfc1c5a1930ed9c7806cebfa2bba3f7885
oneilld136 <oneilld136@gmail.com>
2019-04-22 03:34:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,4 +9,44 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_response = gets.downcase.chomp
 
+  if (1..9).to_a.include?(user_response.to_i)
+    puts "Playing #{songs[user_response.to_i - 1]}"
+    elsif songs.include?(user_response)
+    puts "Playing #{user_response}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+play(songs)
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  #help
+  command = ""
+  while command
+  puts "Please enter a command:"
+  command = gets.downcase.strip
+  case command
+    when 'list'
+      list(songs)
+      when 'play'
+        list(songs)
+        play(songs)
+      when 'help'
+        help
+      when 'exit'
+        exit_jukebox
+        break
+      else
+        help
+      end
+    end
+  end 
